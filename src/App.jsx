0b8122e8764144e1a98e8e97c2310ede969a9043
nirvanafan205 <nirvanafan205@gmail.com>
2024-03-02 19:21:43 -0800

    set up

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./style.css";
+
+const App = () => {
+  return <h1 className="tw-bg-green-300">Hello World</h1>;
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
