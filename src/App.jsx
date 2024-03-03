fc18b9bb6417bf97f1201faefcfe34fd2b596d34
nirvanafan205 <nirvanafan205@gmail.com>
2024-03-03 01:27:46 -0800

    added navbar and layout of web page

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom";
 // import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from "./Components/Footer";
+import Home from "./Pages/Home";
+import Navbar from "./Components/Navbar";
 import "./style.css";
 
 const App = () => {
-  return <Footer />;
+  return (
+    <div className="tw-grid tw-grid-rows-[1fr_auto] tw-grid-cols-[auto_1fr] tw-min-h-screen">
+      <Navbar />
+      <div className="tw-col-span-1 tw-row-span-1">
+        <Home />
+      </div>
+      <div className="tw-col-span-2">
+        <Footer />
+      </div>
+    </div>
+  );
 };
-
 ReactDOM.render(<App />, document.getElementById("root"));
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Navbar = () => {
-  return <h1>Hello World, I am the Navbar</h1>;
+  return (
+    <div className="tw-bg-slate-400 tw-w-64 tw-flex tw-flex-col tw-items-center tw-py-4">
+      <div className="tw-flex tw-flex-row tw-items-center tw-w-full tw-px-4">
+        <img
+          src="/csun_logo.png"
+          alt="CSUN logo"
+          className="tw-h-24 tw-w-24 tw-rounded-full"
+        />
+        <span className="tw-font-bold tw-text-3xl tw-ml-2">Athlete</span>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
