cd05eedab156fc0415869b46e09a86b41ec08dfe
Viper-one-one <taylor.j.nastally@gmail.com>
2024-04-16 21:14:35 -0700

    Add settings section to Settings page

--- src/Pages/Settings.jsx
+++ src/Pages/Settings.jsx
@@ -10,8 +10,24 @@ const Settings = () => {
                 <Navbar />
                 
             </div>
+            {/* Content for general, appearance, notifications, and data settings */}
             <div className="tw-col-span-1 tw-row-span-1 tw-bg-[gradient-radial-1] tw-bg-[gradient-radial-2] tw-border-l-4 tw-border-black">
                 {/* Content for dark mode, notification settings, and health data delete */}
+                <div className='settings-container tw-grid tw-grid-cols-2 tw-gap-4 tw-p-4'>
+                    <div className='settings-item tw-grid tw-grid-cols-2 tw-gap-4 tw-p-4'>
+                        General
+                    </div>
+                    <div className='settings-item tw-grid tw-grid-cols-2 tw-gap-4 tw-p-4'>
+                        Appearance
+                        <button className='settings-button' id='dark-mode-button'>Dark Mode</button>
+                    </div>
+                    <div className='settings-item tw-grid tw-grid-cols-2 tw-gap-4 tw-p-4'>
+                        Notifications
+                    </div>
+                    <div className='settings-item tw-grid tw-grid-cols-2 tw-gap-4 tw-p-4'>
+                        Data Settings
+                    </div>
+                </div>
             </div>
         </div>
     );
