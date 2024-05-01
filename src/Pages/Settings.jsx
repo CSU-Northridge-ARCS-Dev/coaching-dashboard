1abf39eeaa3e40e407d14309f4ae59d79c626e77
Viper-one-one <taylor.j.nastally@gmail.com>
2024-04-30 22:47:27 -0700

    Update Settings page to include notifications UI

--- src/Pages/Settings.jsx
+++ src/Pages/Settings.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBell, faCog } from "@fortawesome/free-solid-svg-icons";
 import Navbar from "../Components/Navbar";
 import Footer from "../Components/Footer";
 import TopBar from '../Components/TopBar';
@@ -50,10 +52,27 @@ const Settings = () => {
 
     return (
         <div className="settings-topbar tw-grid tw-grid-rows-[1fr_auto] tw-grid-cols-[auto_1fr] tw-min-h-screen">
-            <div className="tw-text-xl">
-              Hello {firstName} {lastName}, Welcome back to the
-              <strong> Coaching Dashboard</strong> 👋
+            {isMobile && <Navbar />}
+            {user && !isMobile && (
+            <div className="tw-bg-red-900 tw-flex tw-justify-between tw-items-center tw-px-4 tw-py-2 tw-text-white">
+                <div className="tw-text-xl">
+                Hello {firstName} {lastName}, Welcome back to the
+                <strong> Coaching Dashboard</strong> 👋
+                </div>
+                <div className="tw-flex tw-items-center">
+                <button className="tw-ml-12">
+                    <FontAwesomeIcon icon={faBell} className="tw-text-4xl" />
+                    <div>Alerts</div>
+                </button>
+                <div className="tw-ml-12 tw-flex tw-flex-col tw-items-center">
+                    <Link to="/settings">
+                    <FontAwesomeIcon icon={faCog} className="tw-text-4xl" />
+                    </Link>
+                    <div>Settings</div>
+                </div>
+                </div>
             </div>
+            )}
             {/* Content for general, appearance, notifications, and data settings */}
             <div className="tw-col-span-1 tw-row-span-1 tw-bg-[gradient-radial-1] tw-bg-[gradient-radial-2] tw-border-l-4 tw-border-black">
                 {/* Content for dark mode, notification settings, and health data delete */}
